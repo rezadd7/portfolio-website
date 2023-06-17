a98982d832a1ad4f73103203593708658f5ffe88
reza dehghan <rezadd7@gmail.com>
2023-06-17 19:39:09 +0330

    Create Modal and responsable page

---
+++ src/components/About.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import heroIcon from '../assets/images/hero.svg';
+import CountUp from 'react-countup';
+
+
+const About = () => {
+    return (
+        <section className='pt-0' id='#about'>
+            <div className='container pt-14'>
+                <div className='md:flex md:flex-row items-center justify-between sm:flex-col'>
+                    {/* Main left content */}
+                    <div className='w-full md:basis-1/2'>
+                        <h5 data-aos="fade-right" 
+                            data-aos-duration="1500" 
+                            className='text-headingColor font-[600] text-[16px]'
+                            >
+                            Hello Wellcome
+                        </h5>
+                        <h1 data-aos="fade-up" 
+                            data-aos-duration="1500" 
+                            className='text-headingColor font-[800] text-[1.5rem] sm:text-[40px] leading-[35px]
+                            sm:leading-[46px] mt-5'
+                            >
+                                I'm Reza Dehghan <br />
+                                Front-end Developer
+                            </h1>
+
+                            <div className='flex items-center gap-6 mt-7'
+                                 data-aos="fade-up"
+                                 data-aos-duration="1900"
+                                 data-aos-delay="200"
+                                >
+                                <a href='#contact'>
+                                    <button className='bg-primaryColor text-white font-[500] flex items-center gap-2
+                                        hover:bg-smallTextColor ease-in duration-300 py-2 px-4 rounded-[8px] 
+                                    '><i class="ri-mail-line" ><span className='ml-2'>Here Me</span></i></button>
+                                </a>
+                                <a href='#portfolio' className='text-smallTextColor  font-[600] text-[16px]
+                                    border-b border-solid border-smallTextColor
+                                '>See Portfolio</a>
+                            </div>
+
+                            <p data-aos="fade-left" 
+                                   data-aos-duration="1500"
+                                   className='flex gap-2 text-headingColor mt-12 font-[500] text-[15px] leading-7 sm:pl-14 sm:pr-15' 
+                                >
+                                <span><i class="ri-apps-2-line"></i></span>
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard 
+                                dummy text ever since the 1500s, when an unknown printer took a galley of type and 
+                                scrambled it to make a type specimen book.
+                            </p>
+
+                            <div className='flex items-center gap-9 mt-14'>
+                                <span className='text-smallTextColor text-[15px] font-[600]'>
+                                    Follow me :
+                                </span>
+                                <span>
+                                    <a href='#Linkdin' className='text-smallTextColor text-[18px] font-[600]'><i class="ri-linkedin-box-fill"></i></a>
+                                </span>
+                                <span>
+                                    <a href='#github' className='text-smallTextColor text-[18px] font-[600]'><i class="ri-github-fill"></i></a>
+                                </span>
+                                <span>
+                                    <a href='#instagram' className='text-smallTextColor text-[18px] font-[600]'><i class="ri-instagram-fill"></i></a>
+                                </span>
+                            </div>
+
+                    </div>
+                    {/* Main left content End */}
+
+                    {/* Main image */}
+                    <div className='mt-10 basis-1/3 sm:mt-5'>
+                        <figure className='flex items-center justify-center'>
+                            <img src={heroIcon} alt='Hero'/>
+                        </figure>
+                    </div>
+                    {/* Main image end */}
+
+
+                    {/* Main Count */}
+                    <div className='md:basis-1/5 flex justify-between text-center mt-10 flex-wrap gap-3 
+                        md:mt-0 md:flex-col md:justify-end md:text-end'
+                        >
+                        <div className='mb-10'>
+                            <h2 className='text-smallTextColor font-[700] text-[32px]'>
+                                <CountUp start={0} end={6} duration={2} suffix='+'/>
+                            </h2>
+                            <h4 className='text-smallTextColor font-[600] text-[18px]'>Years of experience</h4>
+                        </div>
+                        <div className='mb-10'>
+                            <h2 className='text-smallTextColor font-[700] text-[32px]'>
+                                <CountUp start={0} end={100} duration={5} suffix='%'/>
+                            </h2>
+                            <h4 className='text-smallTextColor font-[600] text-[18px]'>Success Rate</h4>
+                        </div>
+                        <div className='mb-10'>
+                            <h2 className='text-smallTextColor font-[700] text-[32px]'>
+                                <CountUp start={0} end={149} duration={5} suffix='+'/>
+                            </h2>
+                            <h4 className='text-smallTextColor font-[600] text-[18px]'>Happy Client</h4>
+                        </div>
+                        <div className='mb-10'>
+                            <h2 className='text-smallTextColor font-[700] text-[32px]'>
+                                <CountUp start={0} end={60} duration={5} suffix='+'/>
+                            </h2>
+                            <h4 className='text-smallTextColor font-[600] text-[18px]'>Project Complete</h4>
+                        </div>
+                    </div>
+                    {/* Main Count end */}
+                    
+                </div>
+            </div>
+
+        </section>
+    );
+};
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <section id='#contact' className='pb-16'>
+            <div className="container">
+                <h2 className=' text-headingColor font-[700] text-[2.5rem] mb-8'>Get In Touch</h2>
+                <div className=" md:flex justify-between items-center">
+                    <div className=" w-full md:w-1/2 h-[450px] sm:h-[450]">
+                    <iframe title='google-maps' 
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d207344.07543742796!2d51.182848338456246!3d35.70774021645276!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f8e00491ff3dcd9%3A0xf0b3697c567024bc!2sTehran%2C%20Tehran%20Province%2C%20Iran!5e0!3m2!1sen!2s!4v1686903278922!5m2!1sen!2s" 
+                            className=' border-0 w-full h-full'
+                            allowFullScreen="" 
+                            loading="lazy" 
+                            referrerPolicy="no-referrer-when-downgrade">
+                    </iframe>
+                    </div>
+                    <div className=" w-full mt-8 md:mt-0 md:w-1/2 sm:h-[450px] lg:flex items-center bg-indigo-100 lg:px-8 py-8">
+                        <form className=' w-full'>
+                            <div className=" mb-5">
+                                <input  type="text" 
+                                        placeholder='Enter Your Name' 
+                                        className=' w-full p-3 focus:outline-none rounded-lg'/>
+                            </div>
+                            <div className=" mb-5">
+                                <input  type="text" 
+                                        placeholder='Enter Your Email' 
+                                        className=' w-full p-3 focus:outline-none rounded-lg'/>
+                            </div>
+                            <div className=" mb-5">
+                                <textarea   type="text" 
+                                            rows={3}
+                                            placeholder='Subject' 
+                                            className=' w-full p-3 focus:outline-none rounded-lg'/>
+                            </div>
+
+                            <button className=' w-full p-3 focus:outline-none rounded-[5px] bg-primaryColor hover:bg-headingColor text-white text-center ease-linear duration-150'>Send Message</button>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    )
+    
+};
+
+export default Contact;
